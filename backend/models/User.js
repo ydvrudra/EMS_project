6b13d11916b6a3f4229b86754b7c99efee7dd9b8
ydvrudra <rudrapratapyadav141@gmail.com>
2025-10-28 17:03:59 +0530

    push

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     name: {type: String , require: true},
     email: {type: String , require: true},
     password: {type: String , require: true },
-    role: {type: String , enum: ['admin', 'employee'], default: "admin" },
+    role: {type: String , enum: ['admin', 'employee'], default: "employee" },
     profileImage: {type: String},
     createAt: {type: String, default: Date.now},
     updatedAt: {type: String, default: Date.now},
